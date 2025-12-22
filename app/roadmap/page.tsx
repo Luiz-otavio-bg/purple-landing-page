322b7611a11ab57a9dd33de4387a41efebe8244c
Luiz-otavio-bg <bgoficial2022@gmail.com>
2025-12-22 02:43:46 -0300

    Ajuste do RoadMap

--- app/roadmap/page.tsx
+++ app/roadmap/page.tsx
@@ -57,12 +57,17 @@ const Estrelas = () => {
 
 
 const roadmapSteps = [
-    {id: 1, title: "MODULO DE AGREGAÇÃO", status: "completed", position: "top", description: "TESTE"},
-    {id: 2, title: "MODULO DE AGREGAÇÃO", status: "completed", position: "bottom", description: "teste"},
-    {id: 3, title: "MODULO DE AGREGAÇÃO", status: "completed", position: "top", description: "teste"},
-    {id: 4, title: "MODULO DE AGREGAÇÃO", status: "current", position: "bottom", description: "teste"},
-    {id: 5, title: "MODULO DE AGREGAÇÃO", status: "up coming", position: "top", description: "teste"},
-    {id: 6, title: "MODULO DE AGREGAÇÃO", status: "up coming", position: "bottom", description: "teste"},
+    {id: 1, title: "Core Engine & UI", status: "completed", position: "top", description: "Finalização da interface com darkmode e UX otimizada."},
+    {id: 2, title: "Smart Checkout PIX", status: "completed", position: "bottom", description: "Gateway de pagamento automatizado via PIX integraçado."},
+    {id: 3, title: "Lançamento beta ", status: "up coming", position: "top", description: "Abertura da plataforma para farmácias fundadoras."},
+    {id: 4, title: "Gestão de inventário", status: "up coming", position: "bottom", description: "Alertas de estoque crítico e controle de validades."},
+    {id: 5, title: "Integração SNGPC", status: "up coming", position: "top", description: "Trasmissão de arquivos para controle de medicamentos (Anvisa)."},
+    {id: 6, title: "App de delivery", status: "up coming", position: "bottom", description: "Canal de vendas direto para o cliente final."},
+    {id: 7, title: "BI & Analytics", status: "up coming", position: "top", description: "Relatórios de lucratividades e curva ABC de produtos."},
+    {id: 8, title: "Módulo CRM", status: "up coming", position: "bottom", description: "Sistema de fidelização e retenção de pacientes."},
+    {id: 9, title: "IA Predict", status: "up coming", position: "top", description: "IA para sugestão de compras baseadas em sazonalidade."},
+    {id: 10, title: "API de integração", status: "up coming", position: "bottom", description: "Abertura para integração com convênios e sistemas externos."},
+
 ];
 
 export default function Roadmap (){
@@ -74,7 +79,7 @@ export default function Roadmap (){
             <Estrelas/>
             <div className='container mx-auto px-4'>
                 <div className='text-center mb-32'>
-                    <h2 className='text-5xl md:text-7xl font-bold tracking-tight'> O Caminho para a  <span className='text-purple3'> PURPLE </span></h2>
+                    <h2 className='text-5xl md:text-7xl font-bold tracking-tight'> O Caminho para a  <span className='text-purple3'> Purple </span></h2>
                 </div>
 
                 <div className='hidden md:flex relative max-w-6xl mx-auto h-[400px] items-center'>
@@ -91,20 +96,31 @@ export default function Roadmap (){
                                 onMouseEnter={()=> setHoveredStep(step.id)}
                                 onMouseLeave={()=> setHoveredStep(null)}>
 
+
+
+
+
+                    {// dentro do site o efeito de linha com degrade para cima está como 'bottom' no codigo e vice-versa.
+}
+                                
+                                
                                 <div className={`absolute w-[1px] h-16 bg-gradient-to-b ${step.position === 'top'
-                                    ? 'bottom-1/2 from-transparent to white/40' : 'top-1/2 from-white/40 to-transparent'
+                                    ? 'bottom-1/2 from-transparent to white/0' : 'top-1/2 from-white to-transparent'
+                                }`}/>
+                                <div className={`absolute w-[1px] h-16 bg-gradient-to-b ${step.position === 'bottom'
+                                    ? 'top-1/2 from-transparent to white/0' : 'bottom-1/2 from-transparent to-white'
                                 }`}/>
 
                                 <div className='relative z-30 my-auto cursor-default'>
                                     <motion.div
                                         animate={{ scale: hoveredStep === step.id ? 1.3 : 1 }}
                                         className={`w-7 h-7 rounded-full border-2 flex items-center justify-center bg-white transition-all duration-300
-                                        ${step.status === 'completed' || step.status === 'current' 
+                                        ${step.status === 'completed' || step.status === 'upcoming' 
                                         ? 'border-white shadow-[0_0_15px_rgba(255,255,255,0.8)]' 
-                                        : 'border-white/20'}`}>
+                                        : 'bg-white/0'}`}>
                                         
 
-                                        {step.status === 'current' && (<div className='w-2.5 h-2.5 bg-white rounded-full animate-pulse'/>)}
+                                        {step.status === 'up coming' && (<div className='w-2.5 h-2.5 bg-white/0 rounded-full animate-pulse'/>)}
                                         
 
 
@@ -121,7 +137,7 @@ export default function Roadmap (){
                                             className={`absolute left-1/2 -translate-x-1/2 z-[100] min-w-[220px] max-w-[280px] p-4 rounded-2xl bg-neutral-900/95 backdrop-blur-md border border-purple/30 shadow-[0_20px_50px_rgba(0,0,0,0.8)] text-center
                                             ${step.position === 'top' ? 'top-10' : 'bottom-12'}`}>
 
-                                                <p className='text-sm text-purple leading-relaxed whitespace-normal break-words'>{step.description}</p>
+                                                <p className='text-sm text-white leading-relaxed whitespace-normal break-words'>{step.description}</p>
                                                 <div className={`absolute left-1/2 -translate-x-1/2 w-2 h-2 bg-purple3 border-l border-t border-purple/50 rotate-45
                                                     ${step.position === 'top' ? '-top-1.5' : '-bottom-1.5'}`}></div>
 
@@ -130,8 +146,8 @@ export default function Roadmap (){
                                 </AnimatePresence>
                                 </div>
                                 <div className={`absolute whitespace-nowrap text-center ${step.position === 'top' ? 'bottom-28' : 'top-28'}`}>
-                                        <h3 className={`text-xs md:text-sm font-black tracking-widest uppercase
-                                            ${step.status === 'up coming' ? 'text-white/20' : 'text-white'}`}>
+                                        <h3 className={`text-xs md:text-sm font-black tracking-widest uppercase 
+                                            ${step.status === 'up coming' ? 'text-white/40' : 'text-white'}`}>
                                             {step.title}
                                         </h3>
                                 </div>
